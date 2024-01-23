25d6aee67b8d1e4ba1d7b27f27b9a9f881d18d2a
Lubart2517 <lubomirvihvatniuk@gmail.com>
2024-01-23 20:06:58 +0200

    Add models, make migrations

--- library/models.py
+++ library/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.conf import settings
+from django.utils.translation import gettext as _
 
-# Create your models here.
+
+class Book(models.Model):
+    class Cover(models.TextChoices):
+        HARD = "HARD", _("Hardcover")
+        SOFT = "SOFT", _("Softcover")
+
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    cover = models.CharField(
+        max_length=5, choices=Cover.choices, default=Cover.SOFT
+    )
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField(
+        max_digits=6, decimal_places=2, default=0.00
+    )
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField()
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField(null=True, blank=True)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+
+class Payment(models.Model):
+
+    class StatusChoices(models.TextChoices):
+        PENDING = "PENDING", _("Pending")
+        PAID = "PAID", _("Paid")
+
+    class TypeChoices(models.TextChoices):
+        PAYMENT = "PAYMENT", _("Payment")
+        FINE = "FINE", _("Fine")
+
+    status = models.CharField(max_length=10, choices=StatusChoices.choices)
+    type = models.CharField(max_length=10, choices=TypeChoices.choices)
+    borrowing = models.ForeignKey(Borrowing, on_delete=models.CASCADE)
+    session_url = models.URLField(max_length=200)
+    session_id = models.CharField(max_length=100)
+    money_to_pay = models.DecimalField(max_digits=6, decimal_places=2)
